af0e57538a3816cd1db5f9851662a95b292cf43b
Sasha Berkowsky <snberkowsky@gmail.com>
2022-12-26 13:33:54 -0300

    i stole this from the primeagen

---
+++ .config/nvim/lua/sasza/set.lua
@@ -0,0 +1,24 @@
+local opt = vim.opt
+local g = vim.g
+
+opt.guicursor = ""
+
+opt.nu = true
+opt.relativenumber = true
+opt.cursorline = true
+
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = false
+
+opt.hlsearch = false
+opt.incsearch = true
+
+opt.smartindent = true
+
+opt.scrolloff = 8
+
+opt.updatetime = 50
+
+g.mapleader = " "
