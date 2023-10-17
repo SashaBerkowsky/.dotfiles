48e697c0d2c813134c570007da0db0c91f5adf47
Sasha Berkowsky <s.nberkowsky@gmail.com>
2023-10-17 12:50:46 -0300

    make vim transparent

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,1 +1,11 @@
--- vim.cmd("colorscheme kanagawa-wave")
+-- remove bg
+require('kanagawa').setup({
+  transparent=true;
+})
+-- set theme
+vim.cmd("colorscheme kanagawa-wave")
+-- remove line number bg
+vim.cmd('hi! LineNr guibg=none ctermbg=none')
+-- remove telescope bg
+vim.cmd('hi! TelescopeBorder guibg=none ctermbg=none')
+vim.cmd('hi! TelescopeTitle guibg=none ctermbg=none')
