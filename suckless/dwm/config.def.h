594cf28920078c300993f4f6e5d04d3de477d2a4
Sasha Berkowsky <s.nberkowsky@gmail.com>
2023-10-21 20:40:48 -0300

    font fix

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -10,8 +10,8 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "FiraCodeNerdFont:size=12" };
-static const char dmenufont[]       = "FiraCodeNerdFont:size=12";
+static const char *fonts[]          = { "HackNerdFont:size=14" };
+static const char dmenufont[]       = "HackNerdFont:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
