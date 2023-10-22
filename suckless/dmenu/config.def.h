9105fda155cccdcd35537a2f7f11a2b2a006d692
Sasha Berkowsky <s.nberkowsky@gmail.com>
2023-10-22 01:25:11 -0300

    dmenu size

--- suckless/dmenu/config.def.h
+++ suckless/dmenu/config.def.h
@@ -3,10 +3,10 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int min_width = 250;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"IosevkaNerdFont:size=10"
+	"HackNerdFont:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
