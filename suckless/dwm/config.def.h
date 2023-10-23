7535f91df930953900df4441cad62993f914d7b0
Sasha Berkowsky <s.nberkowsky@gmail.com>
2023-10-23 17:49:29 -0300

    dwm first colors

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -12,17 +12,17 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "HackNerdFont:size=12" };
 static const char dmenufont[]       = "HackNerdFont:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const unsigned int baralpha = 200;
+static const char uns_background[]       = "#222222";
+static const char uns_border[]       = "#444444";
+static const char uns_foreground[]       = "#bbbbbb";
+static const char sel_foreground[]       = "#0C0F0A";
+static const char sel_background[]        = "#C52233";
+static const unsigned int baralpha = 225;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { uns_foreground, uns_background, uns_border },
+	[SchemeSel]  = { sel_foreground, sel_background,  sel_background  },
 };
 static const unsigned int alphas[][3]      = {
     /*               fg      bg        border*/
@@ -69,7 +69,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", uns_background, "-nf", uns_foreground, "-sb", sel_background, "-sf", sel_foreground, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
