e299da867ea7b79f5b2b4b1268f516f96fd1d480
Sasha Berkowsky <s.nberkowsky@gmail.com>
2023-10-23 16:32:14 -0300

    add scrolling to st

--- suckless/st/config.h
+++ suckless/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "HackNerdFont:pixelsize=12:antialias=true:autohint=true";
+static char *font = "HackNerdFont:pixelsize=13:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -204,6 +204,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
 
 /*
