0dd2354246fd47c477442a1520dd9f3d4fc3fd67
Sasha Berkowsky <snberkowsky@gmail.com>
2024-12-06 19:49:35 +0000

    live grep & hide git files

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -4,14 +4,13 @@ local builtin = require('telescope.builtin')
 -- keybindings
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
+vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>pg', builtin.grep_string, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 
 --setup opts
 telescope.setup {
-  defaults = { file_ignore_patterns = {"node_modules"}},
+  defaults = { file_ignore_patterns = {"node_modules", ".git"}},
   pickers = {
     find_files = {
       hidden = true
