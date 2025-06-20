dc2521a5bf315ce36e793c8d74dfc388d94250d5
Sasha Berkowsky <snberkowsky@gmail.com>
2025-06-20 20:21:05 -0300

    copy binding

--- nvim/lua/sasza/remap.lua
+++ nvim/lua/sasza/remap.lua
@@ -4,13 +4,17 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- move lines upwards/downwards
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("v", "<leader>c", '"+y')
-vim.keymap.set("n", "<leader>c", '"+y')
-vim.keymap.set("v", "<leader>p", '"+p')
-vim.keymap.set("n", "<leader>p", '"+p')
+vim.keymap.set("v", "<leader>cp", '"+y')
+vim.keymap.set("n", "<leader>cp", '"+y')
+vim.keymap.set("v", "<leader>pt", '"+p')
+vim.keymap.set("n", "<leader>pt", '"+p')
 
 
 -- keep cursor in the center of the screen when scrolling
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 
+vim.keymap.set("v", "<leader>cp", '"+y')
+vim.keymap.set("n", "<leader>cp", '"+y')
+vim.keymap.set("v", "<leader>pt", '"+p')
+vim.keymap.set("n", "<leader>pt", '"+p')
