d14d53173d030634f706b8962fa8e1daf74d33f9
Sasha Berkowsky <snberkowsky@gmail.com>
2026-09-03 17:06:12 -0300

    fix tresitter

--- nvim/lua/config/plugins/treesitter.lua
+++ nvim/lua/config/plugins/treesitter.lua
@@ -2,16 +2,24 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        branch = "master", -- Pin to master for stable legacy module structure if needed
         build = ":TSUpdate",
         config = function()
-            require("nvim-treesitter.configs").setup({
+            -- Safe require check to give a clear message if lazy loading fails
+            local status, configs = pcall(require, "nvim-treesitter.configs")
+            if not status then
+                vim.notify("nvim-treesitter.configs could not be loaded", vim.log.levels.WARN)
+                return
+            end
+
+            configs.setup({
                 ensure_installed = { "lua", "c", "vim", "markdown", "prolog", "javascript" },
                 auto_install = true,
                 highlight = {
                     enable = true,
-                    aditional_vim_regex_highlighting = false
-                }
+                    additional_vim_regex_highlighting = false, -- Fixed typo (added missing 'd')
+                },
             })
-        end
+        end,
     }
 }
